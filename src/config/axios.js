335365c01e3503a21f5116c5da44df0e60595036
superstahn <superstahn@gmail.com>
2024-01-09 07:28:51 +0100

    edited my axiosInstance link to be that of my hosted web service live link on render

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
-const axiosInstance = axios.create({ baseURL: "http://localhost:4040/api/v1" });
+// const axiosInstance = axios.create({ baseURL: "http://localhost:4040/api/v1" });
+const axiosInstance = axios.create({
+  baseURL: "https://property-listings-api-i6xo.onrender.com/api/v1",
+});
 
 export default axiosInstance;
